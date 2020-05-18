ec7ca6a2b9fccc0d1080ebd4b88539d5a26e75d6
yysach <yysachin11@gmail.com>
2020-05-18 14:57:14 +0530

    create a notebook instance on sageMaker

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
-from aws_cdk import core
+from aws_cdk import (
+    core
+)
 
 from hello_python.hello_python_stack import HelloPythonStack
 
-
 app = core.App()
-HelloPythonStack(app, "hello-python")
+HelloPythonStack(app, "HelloPythonStack", env=core.Environment(
+    region="us-east-1",
+    account="424165949979"))
+
+# MyStack(app, "MyStack", env=core.Environment(
+#        region="REGION",
+#        account="ACCOUNT"))
 
 app.synth()
--- hello_python/hello_python_stack.py
+++ hello_python/hello_python_stack.py
@@ -1,4 +1,7 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_sagemaker as sagemaker,
+    core
+)
 
 
 class HelloPythonStack(core.Stack):
@@ -6,4 +9,11 @@ class HelloPythonStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # enter your code here
+        instance = sagemaker.CfnNotebookInstance(
+            scope=self,
+            id="My instance",
+            instance_type="ml.t2.medium",
+            role_arn=self.node.try_get_context("role-arn"),
+	        default_code_repository="https://git-codecommit.us-east-1.amazonaws.com/v1/repos/AmazonSageMaker-LinearClassificationRepo",
+        )
